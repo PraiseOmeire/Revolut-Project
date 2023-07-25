20be0d3a74c393853a1a09f256aaeb70ff5bd4de
PraiseOmeire <praiseomeire99@gmail.com>
2023-07-25 15:02:35 +0100

    styling

--- src/component/InputSection.tsx
+++ src/component/InputSection.tsx
@@ -1,47 +1,40 @@
 import { useState } from "react";
 import { FiChevronDown } from "react-icons/fi";
 import { NumericFormat } from "react-number-format";
-import { currencies } from "../constants/general";
 import styles from "../styles/inputSection.module.css";
 
 interface Props {
   value: number;
-  onCurrencyChange: (curr: string) => any;
 }
 interface PillProps {
   activeCurrency: string;
   className: string;
-  onCurrencyChange: (curr: string) => any;
 }
 const placeholderText = "0";
 
-export const Pill = ({ activeCurrency, onCurrencyChange }: PillProps) => {
+export const Pill = ({ activeCurrency }: PillProps) => {
   // const tradedCurrencies = Object.values(currencies);
   const [isOpen, setIsOpen] = useState(false);
 
-  const handleClick = (curr: string) => {
-    onCurrencyChange(curr);
-    setIsOpen((prev) => !prev);
-  };
   return (
     <>
       <div>
         <div className={styles.pill} onClick={() => setIsOpen((prev) => !prev)}>
           {activeCurrency} <FiChevronDown className="pt-1" />{" "}
         </div>
-        {isOpen && (
+        {/* {isOpen && (
           <div>
             {Object.values(currencies).map((curr) => (
-              <p onClick={() => handleClick(curr)}>{curr}</p>
+              // <p onClick={() => handleClick(curr)}>{curr}</p>
             ))}
           </div>
-        )}
+        )} */}
       </div>
     </>
   );
 };
 
-export default function InputSection({ value, onCurrencyChange }: Props) {
+export default function InputSection({ value }: Props) {
   return (
     <div>
       <div>
@@ -54,11 +47,7 @@ export default function InputSection({ value, onCurrencyChange }: Props) {
           />
           <div className="border border-solid border-gray-700 rounded-lg pl-2 pr-4 pt-2">
             {" "}
-            <Pill
-              className={styles.pill}
-              activeCurrency={"Eur"}
-              onCurrencyChange={onCurrencyChange}
-            />
+            <Pill className={styles.pill} activeCurrency={"Eur"} />
           </div>
         </div>
       </div>
